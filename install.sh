9b553012e0a18e7028957c97e24c65c532117c21
adityaanilshukla <adityaanilsindhunath@gmailcom>
2025-08-13 12:42:28 +0800

    add autocompletion to zsh config

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ DOTFILES_DIR="$HOME/dotfiles"
 
 # List of dotfiles to symlink: [source relative to dotfiles] [target full path]
 declare -A files=(
-	["zsh/.zshrc"]="$HOME/.zshrc"
-	["tmux/.tmux.conf"]="$HOME/.tmux.conf"
-	["x/.Xresources"]="$HOME/.Xresources"
-	["x/.xprofile"]="$HOME/.xprofile"
-	["gtk/.config/gtk-3.0/settings.ini"]="$HOME/.config/gtk-3.0/settings.ini"
-	["gtk/.config/gtk-4.0/settings.ini"]="$HOME/.config/gtk-4.0/settings.ini"
+	["zsh/zshrc"]="$HOME/.zshrc"
+	["tmux/tmux.conf"]="$HOME/.tmux.conf"
+	["x/Xresources"]="$HOME/.Xresources"
+	["x/xprofile"]="$HOME/.xprofile"
+	["gtk/config/gtk-3.0/settings.ini"]="$HOME/.config/gtk-3.0/settings.ini"
+	["gtk/config/gtk-4.0/settings.ini"]="$HOME/.config/gtk-4.0/settings.ini"
 	["dunst/dunstrc"]="$HOME/.config/dunst/dunstrc"
 	["desktop/shutdown.desktop"]="$HOME/.local/share/applications/shutdown.desktop"
 	["desktop/reboot.desktop"]="$HOME/.local/share/applications/reboot.desktop"
