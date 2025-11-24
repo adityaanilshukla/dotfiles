399a9b8b236dfc355451c88bf229837ea72c858d
adityaanilshukla <adityaanilsindhunath@gmailcom>
2025-11-24 21:19:26 +0800

    add polybar to dotfiles

--- install.sh
+++ install.sh
@@ -14,14 +14,22 @@ declare -A files=(
   ["gtk/config/gtk-3.0/settings.ini"]="$HOME/.config/gtk-3.0/settings.ini"
   ["gtk/config/gtk-4.0/settings.ini"]="$HOME/.config/gtk-4.0/settings.ini"
   ["dunst/dunstrc"]="$HOME/.config/dunst/dunstrc"
+  #desktop scripts
   ["desktop/shutdown.desktop"]="$HOME/.local/share/applications/shutdown.desktop"
   ["desktop/reboot.desktop"]="$HOME/.local/share/applications/reboot.desktop"
+  #vscodium
   ["VScodium/settings.json"]="$HOME/.config/VSCodium/User/settings.json"
   ["VScodium/keybindings.json"]="$HOME/.config/VSCodium/User/keybindings.json"
+
   ["alacritty/alacritty.toml"]="$HOME/.config/alacritty/alacritty.toml"
   ["fastfetch/config.jsonc"]="$HOME/.config/fastfetch/config.jsonc"
   ["koreader/profiles.lua"]="$HOME/.config/koreader/settings/profiles.lua"
   ["rofi/config.rasi"]="$HOME/.config/rofi/config.rasi"
+
+  #polybar
+  ["polybar/config.ini"]="$HOME/.config/polybar/config.ini"
+  ["polybar/polybar-timer.sh"]="$HOME/.config/polybar/polybar-timer.sh"
+  ["polybar/sounds/timer-complete.mp3"]="$HOME/.config/polybar/sounds/timer-complete.mp3"
 )
 
 for src in "${!files[@]}"; do
