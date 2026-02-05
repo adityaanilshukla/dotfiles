afd36b3cac53c3791acd9509e2791a633d8727a7
Aditya Anil Shukla <adityaanilsindhunath@gmail.com>
2026-02-06 00:40:39 +0800

    color scheme changes and add bash script to set adwaita qogir theme

---
+++ gtk-adwaita-dark-qogir.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.desktop.interface icon-theme "Qogir-Dark"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
