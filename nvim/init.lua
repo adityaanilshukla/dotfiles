ca2b2a2df34c824f919e3c026e4a9a735efb727c
Aditya Anil Shukla <aditya.shukla@keysight.com>
2026-03-25 15:10:02 +0800

    update nvim tmux conf and remvoe autocomment from zsh

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,14 +21,18 @@ vim.g.clipboard = {
 
 vim.opt.fillchars:append { eob = " " }
 vim.wo.wrap = false
-vim.o.sessionoptions ="blank,buffers,curdir,folds,help,tabpages,winsize,terminal,localoptions"
+vim.o.sessionoptions ="blank,buffers,curdir,folds,help,tabpages,winsize,localoptions"
+
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldlevel = 0
 
 -- install pugins
 vim.cmd([[
 call plug#begin('~/.config/nvim/plugged')
 
 Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-telescope/telescope.nvim',
+Plug 'nvim-telescope/telescope.nvim'
 
 Plug 'nvim-neo-tree/neo-tree.nvim'
 Plug 'MunifTanjim/nui.nvim'
@@ -60,6 +64,45 @@ require("neo-tree").setup({
   filesystem = {
     filtered_items = {
       hide_gitignored = false,
+    },
+  },
+
+  window = {
+    mappings = {
+      ["Y"] = function(state)
+        local node = state.tree:get_node()
+        local filepath = node:get_id()
+        local filename = node.name
+        local modify = vim.fn.fnamemodify
+
+        local results = {
+          filepath,
+          modify(filepath, ":."),
+          modify(filepath, ":~"),
+          filename,
+          modify(filename, ":r"),
+          modify(filename, ":e"),
+        }
+
+        local i = vim.fn.inputlist({
+          "Choose to copy to clipboard:",
+          "1. Absolute path: " .. results[1],
+          "2. Path relative to CWD: " .. results[2],
+          "3. Path relative to HOME: " .. results[3],
+          "4. Filename: " .. results[4],
+          "5. Filename without extension: " .. results[5],
+          "6. Extension of the filename: " .. results[6],
+        })
+
+        if i > 0 then
+          local result = results[i]
+          if not result then
+            return print("Invalid choice: " .. i)
+          end
+          vim.fn.setreg("+", result)
+          vim.notify("Copied: " .. result)
+        end
+      end,
     },
   },
 })
@@ -105,9 +148,20 @@ vim.keymap.set('n', '<leader>o', function()
   end
 end, opts)
 
+
+local builtin = require("telescope.builtin")
+
 -- Open Telescope find_files with <leader>ff
-map('n', '<leader>ff', ':Telescope find_files<CR>', opts)
-map('n', '<leader>fh', ':Telescope find_files hidden=true<CR>', opts)
+map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", opts)
+map("n", "<leader>fh", "<cmd>Telescope find_files hidden=true<CR>", opts)
+
+map('n', '<leader>fw', "<cmd>lua require('telescope.builtin').live_grep()<CR>", { desc = "Telescope live grep" })
+
+-- map("n", "<leader>ft", function()
+--   builtin.colorscheme({ enable_preview = true })
+-- end, {
+--   desc = "Find themes",
+-- })
 
 -- Toggle comment on a line with <leader>/
 -- This eequires Comment.nvim to be setup
@@ -160,10 +214,10 @@ end
 
 vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
 
-require'nvim-treesitter'.setup {
+require("nvim-treesitter").setup({
   -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
   install_dir = vim.fn.stdpath('data') .. '/site'
-}
+})
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { '<filetype>' },
   callback = function() vim.treesitter.start() end,
